c12c2618c5c8ce68890c0edc663d00094060dbce
smaryam17704 <smaryam17704@gmail.com>
2025-12-26 15:46:11 +0530

    updated db host

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7) # FIX APPLIED
 
 db_user = os.environ.get('DB_USER', 'postgres')
 db_password = os.environ.get('DB_PASSWORD', 'postgres')
-db_host = os.environ.get('DB_HOST', 'localhost')
+db_host = os.environ.get('DB_HOST', 'thenationalscaffolding.cofeca2iwshi.us-east-1.rds.amazonaws.com')
 db_port = os.environ.get('DB_PORT', '5432')
 db_name = os.environ.get('DB_NAME', 'national_scaffolding')
 
